e074eaaf5f2436eb8f012ba2dcc388a3862295f9
AlyonaBasko <33734752+AlyonaBasko@users.noreply.github.com>
2024-04-21 21:40:30 +0200

    Update pixabay-api.js

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -2,18 +2,19 @@ import axios from 'axios';
 
 const API_KEY = '43249978-ed8444d52eae2c923645cb9a6';
 
-export async function searchImages(query, page = 1, perPage = 20) {
+export async function searchImages(query, page = 1, perPage = 15) {
     const params = new URLSearchParams({
         key: API_KEY,
         q: query,
         image_type: 'photo',
         orientation: 'horizontal',
-        safesearch: true
+        safesearch: true,
+        page: page,
+        per_page: perPage
     });
 
     try {
         const response = await axios.get(`https://pixabay.com/api/?${params}`);
-        console.log(response.data);
         return response.data.hits;
     } catch (error) {
         console.error('Error searching images:', error);
